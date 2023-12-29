8ac683bfe59076ad8c896bbf9e92a6b715a9e933
Thomas Marwitz <143403108+ThomasMarwitzQC@users.noreply.github.com>
2023-12-29 16:07:29 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,8 @@ if [[ "$target_platform" == osx-arm64 ]]; then
 		-DCMAKE_INSTALL_LIBDIR=lib \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DCMAKE_VERBOSE_MAKEFILE=ON \
-		-DENABLE_TESTING=Off
+		-DENABLE_TESTING=OFF \
+		-DNNG_TOOLS=ON
 
 else
 	cmake -B build -S . \
@@ -18,9 +19,12 @@ else
 		-DCMAKE_INSTALL_PREFIX=$PREFIX \
 		-DCMAKE_INSTALL_LIBDIR=lib \
 		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_VERBOSE_MAKEFILE=ON
+		-DCMAKE_VERBOSE_MAKEFILE=ON \
+		-DNNG_TOOLS=ON
 fi
 
 cd build
-ninja
+if [[ -z "$MACOSX_DEPLOYMENT_TARGET" ]] && [[ "$target_platform" != "linux-aarch64" ]] && [[ "$target_platform" != "linux-ppc64le" ]]; then
+    ninja test
+fi
 ninja install
